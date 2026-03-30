498a55e69a38f9f1a87b211758f0662e5d28c123
pasalevcuk85-pixel <pasalevcuk85@gmail.com>
2026-03-30 15:55:45 +0200

    Update bot.py
    import os
    import telebot
    
    TOKEN = os.getenv("BOT_TOKEN")
    
    bot = telebot.TeleBot(TOKEN)
    
    @bot.message_handler(commands=['start'])
    def start(message):
        bot.send_message(message.chat.id, "Привіт 👋 Я бот для пошуку роботи")
    
    @bot.message_handler(func=lambda message: True)
    def echo(message):
        bot.send_message(message.chat.id, "Напиши: робота / job / arbeit")
    
    bot.infinity_polling()

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import os
+import telebot
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привіт 👋 Я бот для пошуку роботи")
+
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.send_message(message.chat.id, "Напиши: робота / job / arbeit")
+
+bot.infinity_polling()
